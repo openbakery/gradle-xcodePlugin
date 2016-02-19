aeaac48a49af994e2f3b448dd1540688b98cee19
René Pirringer <rene@pirringer.at>
2016-02-19 09:46:54 +0100

    added that the destination parameter

--- plugin/src/main/groovy/org/openbakery/XcodePlugin.groovy
+++ plugin/src/main/groovy/org/openbakery/XcodePlugin.groovy
@@ -278,6 +278,11 @@ class XcodePlugin implements Plugin<Project> {
 			if (project.hasProperty('xcodebuild.ipaFileName')) {
 				project.xcodebuild.ipaFileName = project['xcodebuild.ipaFileName']
 			}
+			/*
+			if (project.hasProperty('xcodebuild.destination')) {
+				project.xcodebuild.destination = project['xcodebuild.destination']
+			}
+			*/
 
 			if (project.hasProperty('hockeykit.displayName')) {
 				project.hockeykit.displayName = project['hockeykit.displayName']
