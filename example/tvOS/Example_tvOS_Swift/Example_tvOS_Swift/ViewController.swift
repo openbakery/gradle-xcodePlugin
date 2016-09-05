4830abee93edbe70673a571042c07b3e43ccac52
Dominic Freeston <dominic.freeston@gmail.com>
2016-09-05 10:49:12 +0100

    Create a tvOS example project

---
+++ example/tvOS/Example_tvOS_Swift/Example_tvOS_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example_tvOS_Swift
+//
+//  Created by Dominic Freeston on 05/09/2016.
+//  Copyright © 2016 Novoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
