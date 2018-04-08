f7c532d3807e05cb139b2d082aa3f04971cd52f3
Ben Chatelain <ben@octop.ad>
2018-04-07 22:18:15 -0600

    🐛♻️ Add support for xcode build setting transformations
    Only supports a single transformation (I've never seen them chained in a project).
    http://codeworkshop.net/posts/xcode-build-setting-transformations
    
    Also combined the handling of both curly braces ${} and parentheses $() into a single regex. The optional colon-prefixed transformation can be any valid word (\w) characters.

--- plugin/src/main/groovy/org/openbakery/util/VariableResolver.groovy
+++ plugin/src/main/groovy/org/openbakery/util/VariableResolver.groovy
@@ -14,23 +14,14 @@ class VariableResolver {
 	}
 
 	/**
-	 * Replaces the variables ${...}
+	 * Replaces the variables in the given string with the actual value. e.g. ${PRODUCT_NAME} od $(PRODUCT_NAME) get
+	 * replaced by the real product name.
 	 *
-	 * @param text
-	 * @return
-	 */
-	String resolveCurlyBrackets(String text) {
-		String result = text
-		binding().each() { key, value ->
-			if (value != null) {
-				result = result.replaceAll('\\$\\{' + key + '\\}', value)
-			}
-		}
-		return result
-	}
-
-	/**
-	 * Replaces the variables in the given string with the actual value. e.g. ${PRODUCT_NAME} od $(PRODUCT_NAME) get replaced by the real product name
+	 * Also handles a single build settings variable transformation like $(PRODUCT_NAME:c99extidentifier).
+	 * See http://codeworkshop.net/posts/xcode-build-setting-transformations
+	 *
+	 * Note the specified transformation is not applied since we are just replacing the entire variable expression with
+	 * a value from the extension.
 	 *
 	 * @param text
 	 * @return
@@ -43,21 +34,30 @@ class VariableResolver {
 			// Skip resolution if the text doesn't contain any variables.
 			return text
 		}
+
 		String result = text
 		binding().each() { key, value ->
 			if (value != null) {
-				result = result.replaceAll('\\$\\(' + key + '\\)', value)
+				/*
+				 * RegEx pattern matching any of these:
+				 * $(VARIABLE) ${VARIABLE} $(VARIABLE:c99extidentifier)
+				 */
+				String regex = "\\\$(\\(|\\{)$key(:\\w+)?(\\)|\\})"
+				if (!result.matches(regex)) {
+					logger.debug("$result NOT found in $text using regex: $regex")
+				}
+				result = result.replaceAll(regex, value)
 			}
 		}
-		return resolveCurlyBrackets(result)
-	}
 
+		return result
+	}
 
 	def binding() {
 		return [
-						"PRODUCT_NAME": project.xcodebuild.productName,
-						"SRC_ROOT"    : project.projectDir.absolutePath,
-						"TARGET_NAME" : project.xcodebuild.target
+			"PRODUCT_NAME": project.xcodebuild.productName,
+			"SRC_ROOT"    : project.projectDir.absolutePath,
+			"TARGET_NAME" : project.xcodebuild.target
 		]
 	}
 }
