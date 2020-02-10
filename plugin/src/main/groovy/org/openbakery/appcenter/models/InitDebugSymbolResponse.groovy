7ade2f317d5505ba2abf6081ce112532f2d4466d
Bastian Kusserow <bastian.kusserow@intive.com>
2020-02-10 13:10:28 +0100

    ignore unknown fields

--- plugin/src/main/groovy/org/openbakery/appcenter/models/InitDebugSymbolResponse.groovy
+++ plugin/src/main/groovy/org/openbakery/appcenter/models/InitDebugSymbolResponse.groovy
@@ -1,7 +1,9 @@
 package org.openbakery.appcenter.models
 
-class InitDebugSymbolResponse {
+class InitDebugSymbolResponse extends Expando {
 	String symbol_upload_id
 	String upload_url
 	String expiration_date
+
+	def propertyMissing(name, value) {}
 }
