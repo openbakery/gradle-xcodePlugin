8eafac3fa7e5a6d0ad81173eccb8aab1f8a25b71
Rene Pirringer <rene.pirringer@ciqua.com>
2020-03-30 18:36:39 +0200

    updated that Input is not a file

--- plugin/src/main/groovy/org/openbakery/XcodeTestRunTask.groovy
+++ plugin/src/main/groovy/org/openbakery/XcodeTestRunTask.groovy
@@ -2,6 +2,7 @@ package org.openbakery
 
 import groovy.io.FileType
 import org.gradle.api.logging.LogLevel
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Optional
@@ -133,7 +134,7 @@ class XcodeTestRunTask extends AbstractXcodeBuildTask {
 		this.bundleDirectory = bundleDirectory
 	}
 
-	@InputFile
+	@Input
 	@Optional
 	File getBundleDirectory() {
 		if (bundleDirectory instanceof File) {
